043862323accb1a67c5ec94e8500c249af1769e7
kuema <marcokueng@gmx.net>
2024-12-30 17:53:23 +0100

    cleanup planetWorker class

--- public/js/workers/galaxy/proceduralPlanetWorker.js
+++ public/js/workers/galaxy/proceduralPlanetWorker.js
@@ -13,18 +13,20 @@ class ProceduralPlanetWorker {
   }
 
   /**
-   *
-   * @param canvas
+   * @name createPlanet
    * @param radius
    * @param noiseRange
+   * @param stripeFactor
    * @param octavesRange
    * @param lacunarityRange
    * @param persistenceOffset
    * @param baseFrequencyOffset
+   * @param inFrontOfStar
    * @param r
    * @param g
    * @param b
    * @param q
+   * @returns {Promise<Blob>}
    */
   createPlanet=({
                   radius,
@@ -40,10 +42,9 @@ class ProceduralPlanetWorker {
 
     const width = 7 * radius;
     const height = 7 * radius;
-
-    this.#initializeCanvases(width, height, radius);
     this.#valueVector = [];
 
+    this.#initializeCanvases(width, height, radius);
     this.#generateNoise({
       width: width,
       height: height,
@@ -55,7 +56,7 @@ class ProceduralPlanetWorker {
       baseFrequencyOffset: baseFrequencyOffset
     });
 
-    //2d map of the planet surface
+    // 2D map of the planet surface
     this.#drawMap(q,g,q,b, width, height,this.#mapContext);
     this.#wrapSphere(radius, width, height, this.#mapContext, this.#offScreenContext);
     this.#addAtmosphere(r,g,b, radius, this.#offScreenContext);
@@ -65,7 +66,7 @@ class ProceduralPlanetWorker {
     // only draw image where mask is
     this.#offScreenContext.globalCompositeOperation = 'destination-in';
 
-    // draw our circle mask
+    // draw circle mask
     this.#offScreenContext.fillStyle = '#000';
     this.#offScreenContext.beginPath();
     this.#offScreenContext.arc(
@@ -82,13 +83,17 @@ class ProceduralPlanetWorker {
     return this.#offScreenCanvas.convertToBlob();
   }
 
+  /**
+   * @name initializeCanvases
+   * @param width
+   * @param height
+   * @param radius
+   */
   #initializeCanvases = (width, height, radius) => {
-
     this.#mapCanvas = null;
     this.#mapCanvas = new OffscreenCanvas(width, height);
     this.#mapContext = this.#mapCanvas.getContext("2d");
     this.#mapContext.clearRect(0,0,  width, height)
-
     this.#offScreenCanvas = new OffscreenCanvas(radius*2+80, radius*2+80);
     this.#offScreenContext = this.#offScreenCanvas.getContext("2d");
     this.#scaledCanvas = new OffscreenCanvas(2*radius*2+80, 2*radius*2+80);
@@ -97,13 +102,14 @@ class ProceduralPlanetWorker {
   };
 
   /**
-   *
+   * @name generateNoise
    * @param width
    * @param height
    * @param noiseRange
+   * @param stripeFactor
    * @param octavesRange
    * @param lacunarityRange
-   * @param persistenceOffsetas
+   * @param persistenceOffset
    * @param baseFrequencyOffset
    */
   #generateNoise =({
@@ -141,7 +147,7 @@ class ProceduralPlanetWorker {
   }
 
   /**
-   *
+   * @name drawMap
    * @param r
    * @param g
    * @param b
@@ -150,47 +156,43 @@ class ProceduralPlanetWorker {
    * @param height
    * @param ctx
    */
-  #drawMap =(r, g, b, q, width, height, ctx)=>{
-
+  #drawMap = (r, g, b, q, width, height, ctx) => {
     let index = 0;
-    for (let y = 0; y < height; y+=2) {
-      for (let x = 0; x < width; x+=2) {
-        let value = 0;
-        value = this.#valueVector[index];
+    const r1 = r * 1, g1 = g * 3, b1 = b * 4;
 
-        if (value < 1) {
+    for (let y = 0; y < height; y += 2) {
+      for (let x = 0; x < width; x += 2) {
+        const value = this.#valueVector[index++];
+        let fillStyle;
 
-          ctx.fillStyle = `rgba(${Math.floor(value*r*1)}, ${Math.floor(value*g*3)}, ${Math.floor(value*b*4)}, 1)`; // Ocean
-        } else if (value < 2) {
-          ctx.fillStyle = `rgba(${Math.floor(value*r*7)}, ${Math.floor(value*g*7)}, ${Math.floor(value*b*7)}, 1)`; // Ocean
-
+        if (value < 2) {
+          fillStyle = `rgba(${Math.floor(value * r1)}, ${Math.floor(value * g1)}, ${Math.floor(value * b1)}, 1)`;
         } else {
-          //ctx.fillStyle = "#fac";
-          ctx.fillStyle = `rgba(${Math.floor(value * b)}, ${Math.floor(value * r)}, ${Math.floor(value * g)}, 1)`; // Land
+          fillStyle = `rgba(${Math.floor(value * b)}, ${Math.floor(value * r)}, ${Math.floor(value * g)}, 1)`;
         }
 
+        ctx.fillStyle = fillStyle;
         ctx.fillRect(x, y, 2, 2);
-        index++;
       }
     }
-  }
+  };
+
 
   /**
-   *
+   * @name wrapSphere
    * @param radius
    * @param width
    * @param height
    * @param mapCtx
    * @param offScreenCtx
    */
-  #wrapSphere = (radius, width, height, mapCtx, offScreenCtx)=>{
+  #wrapSphere = (radius, width, height, mapCtx, offScreenCtx) => {
     const fromImage = mapCtx.getImageData(0, 0, width, height);
-    const toImage = offScreenCtx.getImageData(0, 0, radius*2, radius*2);
-    const xRotate = Math.PI / 2;
-    const yRotate = 35.3;
-    const image = SphereImageLib.WrapSphere(fromImage, toImage, xRotate, yRotate, radius);
-    offScreenCtx.putImageData(image, 40,40);
-  }
+    const toImage = offScreenCtx.createImageData(radius * 2, radius * 2); // Directly create new ImageData
+    const image = SphereImageLib.WrapSphere(fromImage, toImage, Math.PI / 2, 35.3, radius);
+    offScreenCtx.putImageData(image, 40, 40);
+  };
+
 
   /**
    *
@@ -200,22 +202,32 @@ class ProceduralPlanetWorker {
    * @param radius
    * @param offScreenCtx
    */
-  #addAtmosphere =(r, g, b, radius, offScreenCtx)=>{
-    offScreenCtx.globalAlpha = 0.7;
-    const gradient = this.#offScreenContext.createRadialGradient(
-      radius+40, radius+40, 20,
-      radius+40, radius+40, radius+20
+  #addAtmosphere = (r, g, b, radius, offScreenCtx) => {
+    // Define gradient parameters
+    const centerX = radius + 40;
+    const centerY = radius + 40;
+    const innerRadius = 20;
+    const outerRadius = radius + 20;
+
+    // Create gradient
+    const gradient = offScreenCtx.createRadialGradient(
+      centerX, centerY, innerRadius,
+      centerX, centerY, outerRadius
     );
 
-    gradient.addColorStop(0.0, "rgba("+(r-45)+","+(g-45)+","+(b-45)+", 0.5)");
-    gradient.addColorStop(0.45, "rgba("+(r-15)+","+ (g-15)+"," +(b-15)+", 0.8)");
-    gradient.addColorStop(1, "rgba("+r+15+","+ g+15+"," +b+15+", 1)");
+    gradient.addColorStop(0.0, `rgba(${r - 45}, ${g - 45}, ${b - 45}, 0.5)`);
+    gradient.addColorStop(0.45, `rgba(${r - 15}, ${g - 15}, ${b - 15}, 0.8)`);
+    gradient.addColorStop(1.0, `rgba(${r + 15}, ${g + 15}, ${b + 15}, 1)`);
 
-    offScreenCtx.fillStyle = gradient;
+    // Apply gradient
+    offScreenCtx.save(); // Save canvas state
+    offScreenCtx.globalAlpha = 0.7;
     offScreenCtx.globalCompositeOperation = "source-over";
-    offScreenCtx.fillRect(20, 20, radius*2+30, radius*2+30);
-    offScreenCtx.globalAlpha = 1;
-  }
+    offScreenCtx.fillStyle = gradient;
+    offScreenCtx.fillRect(20, 20, radius * 2 + 30, radius * 2 + 30);
+    offScreenCtx.restore(); // Restore canvas state
+  };
+
 
   /**
    *
@@ -223,29 +235,43 @@ class ProceduralPlanetWorker {
    * @param offScreenCtx
    * @param compositionOperation
    */
-  #addGradient = (radius, offScreenCtx, compositionOperation, inFrontOfStar, r,g,b)=>{
+  #addGradient = (radius, offScreenCtx, compositionOperation, inFrontOfStar, r, g, b) => {
+    offScreenCtx.save(); // Save current state
+
+    // Set global properties
     offScreenCtx.beginPath();
-    offScreenCtx.strokeStyle = "transparent";
     offScreenCtx.globalCompositeOperation = compositionOperation;
-    let gradient = null;
+    offScreenCtx.globalAlpha = 1;
+    offScreenCtx.strokeStyle = "transparent";
+
+    const centerX = radius + 40;
+    const centerY = radius + 40;
+    const gradientWidth = radius * 2 + 40;
+
+    let gradient;
 
     if (inFrontOfStar) {
-      gradient = this.#offScreenContext.createRadialGradient( radius+40,radius+40, 10, radius+40, radius+40, radius+5);
+      // Radial gradient for inFrontOfStar
+      gradient = offScreenCtx.createRadialGradient(centerX, centerY, 10, centerX, centerY, radius + 5);
       gradient.addColorStop(0, "rgba(1, 1, 1, 0.5)");
       gradient.addColorStop(0.97, "rgba(1, 1, 1, 0.5)");
-      gradient.addColorStop(1, 'rgba(155, 155, 155, 0.1)');
+      gradient.addColorStop(1, "rgba(155, 155, 155, 0.1)");
     } else {
-      gradient = this.#offScreenContext.createLinearGradient(0,0,radius*2+40,0);
+      // Linear gradient for behind the star
+      gradient = offScreenCtx.createLinearGradient(0, 0, gradientWidth, 0);
       gradient.addColorStop(0, "rgba(1, 1, 1, 1)");
       gradient.addColorStop(0.55, "rgba(1, 1, 1, 0.9)");
-      gradient.addColorStop(1, `rgba(${r+100},${g+100},${b+100}, 0.6)`);
+      gradient.addColorStop(1, `rgba(${r + 100}, ${g + 100}, ${b + 100}, 0.6)`);
     }
 
-    offScreenCtx.globalAlpha = 1;
+    // Apply the gradient
     offScreenCtx.fillStyle = gradient;
-    offScreenCtx.fillRect(30,30,  radius*2+30,radius*2+30);
+    offScreenCtx.fillRect(30, 30, gradientWidth, gradientWidth);
+
     offScreenCtx.closePath();
-  }
+    offScreenCtx.restore(); // Restore canvas state
+  };
+
 
   /**
    *
